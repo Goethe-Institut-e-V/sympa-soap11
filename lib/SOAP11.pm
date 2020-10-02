e2ad0833f41d10853b9ad2f229f6111dead42a51
Goltz, Immo <immo.goltz@goethe.de>
2020-10-02 14:25:10 +0200

    fixed license and copyright

--- lib/SOAP11.pm
+++ lib/SOAP11.pm
@@ -1,12 +1,12 @@
 # SOAP 1.1 / WSSE compliant interface for
 # Sympa - SYsteme de Multi-Postage Automatique
 #
-# Copyright 2013-2019 Goethe-Institut e.V.
-# Immo Goltz <immo.goltz.extern@goethe.de>
+# Copyright 2013-2020 Goethe-Institut e.V.
+# Immo Goltz <immo.goltz@goethe.de>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
